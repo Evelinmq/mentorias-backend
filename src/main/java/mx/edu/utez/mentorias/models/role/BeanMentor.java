cd43ee3cd5333ea278dde3117e4366c2f54396df
Evelinmq <20243ds170@utez.edu.mx>
2026-03-18 19:38:00 -0600

    Un mentor tiene muchas mentorías

--- src/main/java/mx/edu/utez/mentorias/models/role/BeanMentor.java
+++ src/main/java/mx/edu/utez/mentorias/models/role/BeanMentor.java
@@ -1,7 +1,10 @@
 package mx.edu.utez.mentorias.models.role;
 
 import jakarta.persistence.*;
+import mx.edu.utez.mentorias.models.Mentoria.BeanMentoria;
 import mx.edu.utez.mentorias.models.user.BeanUser;
+
+import java.util.List;
 
 @Entity
 @Table(name = "mentors")
@@ -12,4 +15,7 @@ public class BeanMentor {
     @OneToOne
     @JoinColumn(name = "user_id")
     private BeanUser user;
+
+    @OneToMany(mappedBy = "mentor")
+    private List<BeanMentoria> mentorias;
 }
