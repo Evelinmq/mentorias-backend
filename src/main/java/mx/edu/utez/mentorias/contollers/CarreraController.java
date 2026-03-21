bb05c016e3663517ff704113f3a863f85195c03d
Evelinmq <20243ds170@utez.edu.mx>
2026-03-21 14:30:38 -0600

    Carrera

---
+++ src/main/java/mx/edu/utez/mentorias/contollers/CarreraController.java
@@ -0,0 +1,44 @@
+package mx.edu.utez.mentorias.contollers;
+
+import mx.edu.utez.mentorias.models.Carrera.BeanCarrera;
+import mx.edu.utez.mentorias.services.Carrera.CarreraService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carreras")
+public class CarreraController {
+
+    private final CarreraService carreraService;
+
+    public CarreraController(CarreraService carreraService) {
+        this.carreraService = carreraService;
+    }
+
+    @GetMapping
+    public List<BeanCarrera> listarTodas() {
+        return carreraService.obtenerTodas();
+    }
+
+    // AGREGAR
+    @PostMapping
+    public ResponseEntity<BeanCarrera> agregar(@RequestBody BeanCarrera carrera) {
+        BeanCarrera nuevaCarrera = carreraService.crearCarrera(carrera);
+        return new ResponseEntity<>(nuevaCarrera, HttpStatus.CREATED);
+    }
+
+    // EDITAR
+    @PutMapping("/{id}")
+    public ResponseEntity<BeanCarrera> editar(@PathVariable Long id, @RequestBody BeanCarrera carrera) {
+        try {
+            BeanCarrera actualizada = carreraService.actualizar(id, carrera);
+            return ResponseEntity.ok(actualizada);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
--- src/main/java/mx/edu/utez/mentorias/services/Carrera/CarreraService.java
+++ src/main/java/mx/edu/utez/mentorias/services/Carrera/CarreraService.java
@@ -1,7 +1,10 @@
 package mx.edu.utez.mentorias.services.Carrera;
 
+import mx.edu.utez.mentorias.models.Carrera.BeanCarrera;
 import mx.edu.utez.mentorias.models.Carrera.CarreraRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarreraService {
@@ -11,5 +14,21 @@ public class CarreraService {
     public CarreraService(CarreraRepository carreraRepository) {
         this.carreraRepository = carreraRepository;
     }
+
+    public List<BeanCarrera> obtenerTodas() {
+        return carreraRepository.findAll();
+    }
+
+    public BeanCarrera crearCarrera(BeanCarrera carrera) {
+        return carreraRepository.save(carrera);
+    }
+
+    public BeanCarrera actualizar(Long id, BeanCarrera datosActualizados) {
+        return carreraRepository.findById(id).map(carrera -> {
+            carrera.setNombre(datosActualizados.getNombre());
+            return carreraRepository.save(carrera);
+        }).orElseThrow(() -> new RuntimeException("Carrera no encontrada con id: " + id));
+    }
+
 
 }
