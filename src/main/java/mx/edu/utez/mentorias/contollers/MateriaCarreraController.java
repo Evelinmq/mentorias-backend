cb79721de95f03570823e78376aba7b25d1c23fd
Evelinmq <20243ds170@utez.edu.mx>
2026-03-21 22:33:20 -0600

    Vinculación de materias con carreras

---
+++ src/main/java/mx/edu/utez/mentorias/contollers/MateriaCarreraController.java
@@ -0,0 +1,38 @@
+package mx.edu.utez.mentorias.contollers;
+
+import mx.edu.utez.mentorias.models.MateriaCarrera.BeanMateriaCarrera;
+import mx.edu.utez.mentorias.services.MateriaCarrera.MateriaCarreraService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/materia-carrera")
+public class MateriaCarreraController {
+
+    private final MateriaCarreraService materiaCarreraService;
+
+    public MateriaCarreraController(MateriaCarreraService materiaCarreraService) {
+        this.materiaCarreraService = materiaCarreraService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BeanMateriaCarrera>> listar() {
+        return ResponseEntity.ok(materiaCarreraService.listarTodas());
+    }
+
+    // ASOCIAR
+    @PostMapping
+    public ResponseEntity<BeanMateriaCarrera> asociar(@RequestBody BeanMateriaCarrera union) {
+        return new ResponseEntity<>(materiaCarreraService.guardar(union), HttpStatus.CREATED);
+    }
+
+    // DESVINCULAR
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> desvincular(@PathVariable Long id) {
+        materiaCarreraService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/mx/edu/utez/mentorias/services/MateriaCarrera/MateriaCarreraService.java
+++ src/main/java/mx/edu/utez/mentorias/services/MateriaCarrera/MateriaCarreraService.java
@@ -1,8 +1,11 @@
 package mx.edu.utez.mentorias.services.MateriaCarrera;
 
 
+import mx.edu.utez.mentorias.models.MateriaCarrera.BeanMateriaCarrera;
 import mx.edu.utez.mentorias.models.MateriaCarrera.MateriaCarreraRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MateriaCarreraService {
@@ -12,4 +15,21 @@ public class MateriaCarreraService {
     public MateriaCarreraService(MateriaCarreraRepository materiaCarreraRepository) {
         this.materiaCarreraRepository = materiaCarreraRepository;
     }
+    public List<BeanMateriaCarrera> listarTodas() {
+        return materiaCarreraRepository.findAll();
+    }
+
+    // Crear vinculación materia con carrera
+    public BeanMateriaCarrera guardar(BeanMateriaCarrera union) {
+        return materiaCarreraRepository.save(union);
+    }
+
+    // Eliminar la vinculación
+    public void eliminar(Long id) {
+        if (!materiaCarreraRepository.existsById(id)) {
+            throw new RuntimeException("La asociación con ID " + id + " no existe.");
+        }
+        materiaCarreraRepository.deleteById(id);
+    }
+
 }
