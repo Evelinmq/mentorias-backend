b9478f950d137c268c1c39c60cb6a2047a0ac313
Andreaara <20243ds171@utez.edu.mx>
2026-03-25 18:23:19 -0600

    .

--- src/main/java/mx/edu/utez/mentorias/contollers/CarreraController.java
+++ src/main/java/mx/edu/utez/mentorias/contollers/CarreraController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/carreras")
-@CrossOrigin(origins = "http://localhost:5173")
 public class CarreraController {
 
     private final CarreraService carreraService;
