e950338464ce0385afdfd8d4d00730b9cff1ccfd
Sundown_uwu <20243ds148@utez.edu>
2026-03-26 13:15:27 -0600

    Logica del login por roles completa

--- src/main/java/mx/edu/utez/mentorias/models/UsuarioRol/UsuarioRolRepository.java
+++ src/main/java/mx/edu/utez/mentorias/models/UsuarioRol/UsuarioRolRepository.java
@@ -3,6 +3,12 @@ package mx.edu.utez.mentorias.models.UsuarioRol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRolRepository extends JpaRepository<BeanUsuarioRol, Long> {
+
+    // Busca los roles de un usuario por su ID
+    List<BeanUsuarioRol> findByUsuario_Id(Long usuarioId);
+
 }
