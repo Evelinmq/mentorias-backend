f29e44592499725427d79c91d2171b1e53b380e4
Evelinmq <20243ds170@utez.edu.mx>
2026-04-11 15:28:07 -0600

    Pequeño cambio

--- src/main/java/mx/edu/utez/mentorias/models/Edificio/BeanEdificio.java
+++ src/main/java/mx/edu/utez/mentorias/models/Edificio/BeanEdificio.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import mx.edu.utez.mentorias.models.Espacio.BeanEspacio;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import java.util.List;
 
